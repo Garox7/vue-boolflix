27661fde1d4641a4c2ce7d186f2f82759d3e3f08
Garox7 <110902920+Garox7@users.noreply.github.com>
2022-12-01 00:22:42 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <HeaderApp />
+    <MainApp />
+  </div>
+</template>
+
+<script>
+import HeaderApp from '@/components/HeaderApp.vue';
+import MainApp from '@/components/MainApp.vue';
+
+export default {
+  components: {
+    HeaderApp,
+    MainApp,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
