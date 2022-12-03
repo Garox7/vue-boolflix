0cd3705fec5f012c98708f7b62be090bde36e504
Garox7 <110902920+Garox7@users.noreply.github.com>
2022-12-03 18:27:39 +0100

    Header Improvements

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,18 +1,23 @@
 <template>
   <header>
     <a class="logo"><h1>BOOLFLIX</h1></a>
-    <div>
-      <form @submit.prevent="$emit('Searching', searchString)">
-        <label for="input-search">
-          <input
-            type="text"
-            id="input-search"
-            placeholder="Titoli, persone, generi"
-            v-model="searchString"
-          >
-        </label>
-        <button>Cerca</button>
-      </form>
+    <nav class="navigation">
+      <a href="">Home</a>
+      <a href="">Serie TV</a>
+      <a href="">Film</a>
+    </nav>
+    <div class="secondary__navigation">
+      <label for="input-search">
+        <input
+          type="text"
+          id="input-search"
+          placeholder="Titoli, persone, generi"
+          v-model="searchString"
+        >
+      </label>
+      <button @click="$emit('Searching', searchString)">
+        Cerca
+      </button>
     </div>
   </header>
 </template>
@@ -40,12 +45,35 @@ header {
   height: $header-h;
   padding: 0 4%;
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  z-index: 10;
+  z-index: 20;
 
   .logo {
     color: $header-logo-color;
+    margin-right: 30px;
+  }
+
+  .navigation {
+    flex: 1 1 auto;
+    display: flex;
+    gap: 1rem;
+
+    a {
+      color: $header-link-color;
+      font-size: 14px;
+      text-decoration: none;
+      transition: all .2s ease-in-out;
+
+      &:hover {
+        color: $header-link-color-h;
+      }
+
+      &:active {
+        color: #fff;
+        text-decoration: underline;
+      }
+    }
+
   }
 }
 </style>
